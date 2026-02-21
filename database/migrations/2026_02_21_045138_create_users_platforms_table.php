4cfc7e9e903dbc7708747587d2683ac0b23d0474
Arturo Morales <arturo.m.pedroza@gmail.com>
2026-02-21 16:29:39 -0600

    Token added to users-platforms-table

--- database/migrations/2026_02_21_045138_create_users_platforms_table.php
+++ database/migrations/2026_02_21_045138_create_users_platforms_table.php
@@ -18,6 +18,9 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('platform_id')->constrained()->onDelete('cascade');
 
+            //token for authentication
+            $table->string('token')->unique(); 
+
             //constraints for unique user-platform combination
             $table->unique(['user_id', 'platform_id']); //Un usuario no puede tener repetida la misma plataforma
             
